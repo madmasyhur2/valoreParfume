4153a2514abecfc78456250a5202028697993740
madmasyhur2 <muhammadmasyhur2@gmail.com>
2024-02-22 11:13:40 +0700

    migration

---
+++ shipping/app/Http/Controllers/ZipcodeController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\zipcode;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorezipcodeRequest;
+use App\Http\Requests\UpdatezipcodeRequest;
+
+class ZipcodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorezipcodeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(zipcode $zipcode)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(zipcode $zipcode)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatezipcodeRequest $request, zipcode $zipcode)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(zipcode $zipcode)
+    {
+        //
+    }
+}
