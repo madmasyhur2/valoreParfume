0e77db1cc773fbd16a695f4b9eeb0dbdc6d77083
madmasyhur2 <muhammadmasyhur2@gmail.com>
2024-02-22 13:12:39 +0700

    rename and data seed

--- shipping/resources/views/home.blade.php
+++ shipping/resources/views/home.blade.php
@@ -19,7 +19,7 @@
                 <input type="text" id="destination" name="destination" placeholder="Kota Tujuan" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required>
             </div>
             <div class="mb-4">
-                <label for="weight" class="block text-sm font-medium text-gray-700">Berat (gram)</label>
+                <label for="weight" class="block text-sm font-medium text-gray-700">Berat (Gram)</label>
                 <input type="text" id="weight" name="weight" value="1000" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required>
             </div>
             <div class="mb-4">
